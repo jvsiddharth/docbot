449050821ad87892f3ba4bc0f943175f84f75c98
Siddharth Jadhav <104616265+jvsiddharth@users.noreply.github.com>
2023-08-17 21:04:38 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -9,32 +9,16 @@ css = '''
 .chat-message.bot {
     background-color: #475063
 }
-.chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
-}
-.chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
-}
 '''
 
 bot_template = '''
 <div class="chat-message bot">
-    </div>    
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
-<div class="chat-message user">
-    </div>    
+<div class="chat-message user"> 
     <div class="message">{{MSG}}</div>
 </div>
 '''
